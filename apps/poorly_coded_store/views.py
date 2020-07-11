dc6166f8d6d55736eaee5c3cbcda57447db560df
Shari Bradford <shari.bradford@gmail.com>
2020-07-10 21:26:29 -0400

    added additional information to Views.checkout and checkout.html

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -8,9 +8,25 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    print("POST data")
+    print(request.POST)
+
     quantity_from_form = int(request.POST["quantity"])
     price_from_form = float(request.POST["price"])
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    
+    this_order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    total_quantity = 0
+    total_amount = 0
+    for order in Order.objects.all():
+        total_quantity += order.quantity_ordered
+        total_amount += order.total_price
+
+    context = {
+        order: this_order
+        total_amount : total_amount,
+        total_quantity: total_quantity
+    }
+
+    return render(request, "store/checkout.html",context)
