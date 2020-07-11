6055a22f3b8fa004d6dc647d9d6275062b2d54b2
Shari Bradford <shari.bradford@gmail.com>
2020-07-10 22:05:46 -0400

    fixed issues with refreshing page causing additional charges, prevented users from changing price.

--- apps/poorly_coded_store/urls.py
+++ apps/poorly_coded_store/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^checkout$', views.checkout)
+    url(r'^checkout$', views.checkout),
+    path('confirm/<int:order_id>', views.confirm_order)
 ]
--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
+context = {}
+
 def index(request):
-    context = {
-        "all_products": Product.objects.all()
-    }
+    context["all_products"] = Product.objects.all()
     return render(request, "store/index.html", context)
 
 def checkout(request):
@@ -12,21 +12,22 @@ def checkout(request):
     print(request.POST)
 
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
+    #price_from_form = float(request.POST["price"])
+    lookup_price = Product.objects.get(id=request.POST["product_id"]).price
+    total_charge = quantity_from_form * lookup_price
+    print(f"Charging credit card... {total_charge}")
     
     this_order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect(f"/confirm/{this_order.id}")
+
+def confirm_order(request,order_id):
     total_quantity = 0
     total_amount = 0
     for order in Order.objects.all():
         total_quantity += order.quantity_ordered
         total_amount += order.total_price
 
-    context = {
-        order: this_order
-        total_amount : total_amount,
-        total_quantity: total_quantity
-    }
-
-    return render(request, "store/checkout.html",context)
+    context["order"] = Order.objects.get(id=order_id)
+    context["total_amount"] = total_amount
+    context["total_quantity"] =  total_quantity
+    return render(request, "store/checkout.html", context)
